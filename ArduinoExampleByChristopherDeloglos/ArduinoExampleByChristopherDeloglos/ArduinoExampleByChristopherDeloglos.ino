6976f95c8d51901b42ca9ce57945040a5d7d73e3
DESKTOP-2F3FBQ4\ChristopherDeloglos <cd.dman@gmail.com>
2017-02-10 15:44:31 -0500

    Example Arduino Project Creation

---
+++ ArduinoExampleByChristopherDeloglos/ArduinoExampleByChristopherDeloglos/ArduinoExampleByChristopherDeloglos.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoExampleByChristopherDeloglos.ino
+ Created:	2/10/2017 3:43:19 PM
+ Author:	ChristopherDeloglos
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin();
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	Serial.println("Christopher Made this because he's a boss");
+}
