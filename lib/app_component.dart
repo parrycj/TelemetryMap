2ca567ded8c9ab4419894a528666391284cf62e7
parrycj <parrycj@vcu.edu>
2017-02-16 23:27:57 -0500

    google maps showing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,13 +4,10 @@
 import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
-import 'package:Telemetry/hello_dialog/hello_dialog.dart';
 
 @Component(
   selector: 'my-app',
-  styleUrls: const ['app_component.css'],
-  templateUrl: 'app_component.html',
-  directives: const [materialDirectives, HelloDialog],
+  directives: const [materialDirectives],
   providers: const [materialProviders],
 )
 class AppComponent {
